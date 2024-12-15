75899a40c046abfeb5741ea42d040a02b1c189df
hubgit5487301 <vikasyy5487301@gmail.com>
2024-12-15 15:51:29 +0530

    checking again

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -2,7 +2,6 @@ import orderModel from "../models/orderModel.js";
 import userModel from "../models/userModel.js"
 import Stripe from "stripe";
 const url = process.env.origin;
-console.log(url)
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+const url = process.env.origin
+console.log(url)
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
       // When the React app calls /api, proxy the request to the backend
-      '/api': 'http://localhost:4000',
+      '/api': url,
       // You can add additional proxies for other API routes here
       // '/another-api': 'http://localhost:4001',
     }
