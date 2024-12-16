90ac2006e977bc56473aa52975e88b70cc2dc42e
hubgit5487301 <vikasyy5487301@gmail.com>
2024-12-16 11:39:20 +0530

    added admin panel

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -10,7 +10,7 @@ import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
 
-     const url ="http://localhost:4000"
+     const url ="https://food-del1-t73r.onrender.com"
    
   return (
     <div>
